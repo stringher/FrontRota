4ca1d0f85d17bb3f8b723c18004e7ff510277224
rcostajr <roberto.cjunior@spread.com.br>
2022-01-11 13:30:51 -0300

    feat: Player Screen

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,20 +1,15 @@
 import React from 'react'
+import { ActivityIndicator } from 'react-native';
 import { Audio } from 'expo-av';
 import styled from "styled-components/native";
 import { MaterialIcons } from '@expo/vector-icons'
 import AppLoading from 'expo-app-loading';
-import { Dimensions } from "react-native";
+import { useFonts, RobotoCondensed_300Light } from '@expo-google-fonts/roboto-condensed';
 
 const Container = styled.View`
     flex: 1;  
     flex-direction: column;
     align-items: center;
-    justify-content: space-between;
-    width: 150px;
-    background-color: #132440;
-    border-width: 1px;
-    border-color: white;
-    height: 300px;
 `;
 
 const ButtonContainer = styled.View`
@@ -23,84 +18,164 @@ const ButtonContainer = styled.View`
     align-items: center;
     justify-content: space-between;
     width: 150px;
-    background-color: #132440;
-    border-width: 1px;
-    border-color: white;
 `;
 
 const Button = styled.TouchableHighlight`
     border-radius: 8px;
-    /* border-width: 1;
-    border-color: white; */
-    
+`
+const TimeLabel = styled.Text`
+    color: #BBBBBB;
+    font-size: 12px;
+    font-family: RobotoCondensed_300Light;
 `
 
+// const Progress = (data) => {
 
-const Progress = (data) => {
+//     return (
+//         <></>
+//         // <ProgressBar progress={0.5} color={Colors.red800} />
+//         // <ProgressBar
+//         //     styleAttr="Horizontal"
+//         //     color="#2196F3"
+//         //     indeterminate={false}
+//         //     progress={0.5}
+//         // />
+//     )
+// }
 
-    return (
-        <></>
-        // <ProgressBar progress={0.5} color={Colors.red800} />
-        // <ProgressBar
-        //     styleAttr="Horizontal"
-        //     color="#2196F3"
-        //     indeterminate={false}
-        //     progress={0.5}
-        // />
-    )
+
+const msToTime = (duration) => {
+    // let milliseconds = Math.floor((duration % 1000) / 100);
+    let seconds = Math.floor((duration / 1000) % 60);
+    let minutes = Math.floor((duration / (1000 * 60)) % 60);
+    let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    // return (
+    //     ((hours === "00") ? "" : hours + ":h") +
+    //     ((minutes === "00" && hours === "00") ? "" : minutes + ":") +
+    //     ((seconds === '00' && minutes === "00" && hours === "00") ? "" : seconds)
+    // )
+    return hours + ":" + minutes + ":" + seconds;
 }
 
 const AudioPlayer = (props) => {
-    const [status, setStatus] = React.useState('Stopped');
+    const [fontsLoaded] = useFonts({ RobotoCondensed_300Light, });
+
     const [sound, setSound] = React.useState();
-
-    React.useEffect(() => {
-        switch (status) {
-            case 'Stopped':
-                break;
-            case 'Playing':
-                break;
-            case 'Paused':
-                break;
-
-            default:
-                break;
-        }
-    }, [status]);
+    const [status, setStatus] = React.useState('Stopped');
+    const [loading, setLoading] = React.useState(false);
+    const [duration, setDuration] = React.useState(0);
+    const [position, setPosition] = React.useState(0);
 
     React.useEffect(() => {
         return sound ? () => sound.unloadAsync() : undefined;
     }, [sound]);
 
-
     const playSound = async () => {
-        if (props.url) {
-            const { sound } = await Audio.Sound.createAsync(props.url);
-            setSound(sound);
-            await sound.playAsync().then(x => {
-                console.log('Playing');
-                setStatus('Playing')
-            });
+        if (props.url && status !== 'Error') {
+            if (sound) {
+                if (status === 'Playing') {
+                    sound.pauseAsync();
+                    setStatus('Paused');
+                } else if (status === 'Paused') {
+                    sound.setStatusAsync({ shouldPlay: true });
+                }
+            } else {
+                try {
+                    setStatus('Loading')
+                    const { sound } = await Audio.Sound.createAsync({ uri: props.url }, { shouldPlay: true });
+                    setSound(sound);
+                    await sound.playAsync();
+                    sound.setOnPlaybackStatusUpdate(async (s) => {
+                        setDuration(s.durationMillis);
+                        setPosition(s.positionMillis);
+                        if (s.loading !== loading) setLoading(s.loading)
+                        if (s.isPlaying && status !== 'Playing') setStatus('Playing');
+                        // didJustFinish: false
+                        // durationMillis: 1533178.776
+                        // isBuffering: false
+                        // isLoaded: true
+                        // isLooping: false
+                        // isMuted: false
+                        // isPlaying: true
+                        // positionMillis: 24617.835
+                        // progressUpdateIntervalMillis: 100
+                        // rate: 1
+                        // shouldCorrectPitch: false
+                        // shouldPlay: false
+                        // uri: "https://traffic.omny.fm/d/clips/f895e4af-2068-409d-a7a7-aa9201219358/19cc81e9-b5ee-4bfa-b607-ab95010bd308/fe9315e4-684c-4e8a-8b57-aba30117a422/audio.mp3"
+                        // volume: 1
+                    })
+                } catch { setStatus('Error') }
+
+            }
         }
     }
 
-    return (
-        <Container>
-            <Progress data={0.50} />
-            <ButtonContainer>
-                <Button >
-                    <MaterialIcons name="replay-10" size={30} color='white' />
-                </Button>
-                <Button onPress={playSound}>
-                    <MaterialIcons name="play-circle-outline" size={50} color='white' />
-                </Button>
-                <Button>
-                    <MaterialIcons name="forward-30" size={30} color='white' />
-                </Button>
-            </ButtonContainer>
-        </Container>
-    )
-}
+    const forward = (value = 30) => {
+        if (sound) {
+            if (position + (value * 1000) < duration) {
+                sound.setStatusAsync({ positionMillis: position + (value * 1000), shouldPlay: true });
+            }
+        }
+    }
 
+    const backward = (value = 10) => {
+        if (sound) {
+            if (position > (value * 1000)) {
+                sound.setStatusAsync({ positionMillis: position - (value * 1000), shouldPlay: true });
+            } else if (position <= (value * 1000)) {
+                sound.setStatusAsync({ positionMillis: 1, shouldPlay: true });
+            }
+        }
+    }
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            <Container>
+                {(!(status === 'Playing' || status === 'Paused' || status === 'Stopped') || loading) && <ActivityIndicator />}
+                {/* <Progress data={0.50} /> */}
+                <ButtonContainer>
+                    {position > 0 && <TimeLabel>{msToTime(position || 0)}</TimeLabel>}
+                    {duration > 0 && <TimeLabel>{msToTime(duration - position || 0)}</TimeLabel>}
+                </ButtonContainer>
+                <ButtonContainer>
+                    <Button
+                        disabled={!(status === 'Playing' || status === 'Paused')}
+                        underlayColor="#DDDDDD"
+                        onPress={() => backward(10)}
+                    >
+                        <MaterialIcons name="replay-10" size={30} color={(status === 'Playing' || status === 'Paused') ? 'white' : 'gray'} />
+                    </Button>
+                    <Button
+                        disabled={!(status === 'Playing' || status === 'Paused' || status === 'Stopped')}
+                        underlayColor="#DDDDDD"
+                        onPress={playSound}
+                    >
+                        {
+                            status === 'Playing' ?
+                                <MaterialIcons name="pause-circle-outline" size={50} color={(status === 'Playing' || status === 'Paused' || status === 'Stopped') ? 'white' : 'gray'} />
+                                :
+                                <MaterialIcons name="play-circle-outline" size={50} color={(status === 'Playing' || status === 'Paused' || status === 'Stopped') ? 'white' : 'gray'} />
+                        }
+                    </Button>
+                    <Button
+                        disabled={status === 'Error' || status === 'Loading'}
+                        underlayColor="#DDDDDD"
+                        onPress={() => forward(30)}
+                    >
+                        <MaterialIcons name="forward-30" size={30} color={(status === 'Playing' || status === 'Paused') ? 'white' : 'gray'} />
+                    </Button>
+                </ButtonContainer>
+            </Container>
+        )
+    }
+}
 
 export default AudioPlayer;
--- pages/player/PlayerScreen.js
+++ pages/player/PlayerScreen.js
@@ -1,19 +1,76 @@
 import React from "react";
+import styled from "styled-components/native";
 import AudioPlayer from "../../components/AudioPlayer";
 import TitlePageTemplate from "../../components/TitlePageTemplate";
+import AppLoading from 'expo-app-loading';
+import { useFonts, RobotoCondensed_300Light } from '@expo-google-fonts/roboto-condensed';
 
+import { Dimensions } from "react-native";
 
-const PlayListScreen = (props) => {
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+const Container = styled.View`
+    background-color: #132440;
+    align-items: center;
+    justify-content: center;
+    top: -30px;
+    width: ${width - 50}px;
+    height: ${height - 180}px;
+`;
+
+const ImageContainer = styled.View`
+    width: ${width - 50}px;
+    height: ${width - 50}px;
+    border-radius: 10px;
+    border-width: 1px;
+    border-color: white;
+    margin-bottom: 25px;
+`;
+
+const AudioContainer = styled.View`
+    /* margin-top: ${height - 250}px; */
+    background-color: #132440;
+    height: 70px;
+    /* border-width: 1px;
+    border-color: white; */
+`;
+
+const Label = styled.Text`
+    font-size: 18px;
+    font-family: RobotoCondensed_300Light;
+    color: #F6F6F6;
+    font-weight: 400;
+    margin-bottom: 15px;
+`
+
+const url = 'https://traffic.omny.fm/d/clips/f895e4af-2068-409d-a7a7-aa9201219358/19cc81e9-b5ee-4bfa-b607-ab95010bd308/fe9315e4-684c-4e8a-8b57-aba30117a422/audio.mp3'
+const PlayerScreen = (props) => {
+
+    const [fontsLoaded] = useFonts({ RobotoCondensed_300Light, });
 
     const data = props?.route?.params;
 
-    return (
-        <TitlePageTemplate {...props} nome={""} footerId={0} >
-            <AudioPlayer url='' />
-        </TitlePageTemplate>
-    )
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            <TitlePageTemplate {...props} nome={""} footerId={0} >
+                <Container>
+                    <ImageContainer>
+                    </ImageContainer>
+                    <Label>
+                        {props.label}
+                    </Label>
+                    <AudioContainer>
+                        <AudioPlayer url={data?.url || url} />
+                    </AudioContainer>
+                </Container>
+            </TitlePageTemplate>
+        )
+    }
 
 }
 
-export default PlayListScreen;
+export default PlayerScreen;
 
